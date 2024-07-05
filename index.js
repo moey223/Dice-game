c6e5006dc67873f5cae15ab6cc52f7b3be85f23b
moey223 <168573066+moey223@users.noreply.github.com>
2024-07-05 11:33:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,31 +5,31 @@ function play() {
     const image2 = document.querySelector("#Player2");
     /*image changing for player 1 */
     if(randNum1 == 1){
-        image1.src = './images/dice1.png';
+        image1.src = './dice1.png';
     } else if(randNum1 == 2){
-        image1.src = './images/dice2.png';
+        image1.src = './dice2.png';
     } else if(randNum1 == 3){
-        image1.src = './images/dice3.png';
+        image1.src = './dice3.png';
     } else if(randNum1 == 4){
-        image1.src = './images/dice4.png';
+        image1.src = './dice4.png';
     } else if(randNum1 == 5){
-        image1.src = './images/dice5.png';
+        image1.src = './dice5.png';
     } else if(randNum1 == 6){
-        image1.src = './images/dice6.png';
+        image1.src = './dice6.png';
     }
     /*image changing for player 2 */
     if(randNum2 == 1){
-        image2.src = './images/dice1.png';
+        image2.src = './dice1.png';
     } else if(randNum2 == 2){
-        image2.src = './images/dice2.png';
+        image2.src = './dice2.png';
     } else if(randNum2 == 3){
-        image2.src = './images/dice3.png';
+        image2.src = './dice3.png';
     } else if(randNum2 == 4){
-        image2.src = './images/dice4.png';
+        image2.src = './dice4.png';
     } else if(randNum2 == 5){
-        image2.src = './images/dice5.png';
+        image2.src = './dice5.png';
     } else if(randNum2 == 6){
-        image2.src = './images/dice6.png';
+        image2.src = './dice6.png';
     }
     if(randNum1 >randNum2 ){
         document.querySelector("#winnerfr").innerHTML = "Player 1 wins!";
