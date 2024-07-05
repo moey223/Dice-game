9ae8271b770574b0a3674e18a2b303cd6e3c8ff5
moey223 <168573066+moey223@users.noreply.github.com>
2024-07-05 11:27:14 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+function play() {
+    var randNum1 = Math.floor(Math.random() * 6) + 1;
+    var randNum2 = Math.floor(Math.random() * 6) + 1;
+    const image1 = document.querySelector("#Player1");
+    const image2 = document.querySelector("#Player2");
+    /*image changing for player 1 */
+    if(randNum1 == 1){
+        image1.src = './images/dice1.png';
+    } else if(randNum1 == 2){
+        image1.src = './images/dice2.png';
+    } else if(randNum1 == 3){
+        image1.src = './images/dice3.png';
+    } else if(randNum1 == 4){
+        image1.src = './images/dice4.png';
+    } else if(randNum1 == 5){
+        image1.src = './images/dice5.png';
+    } else if(randNum1 == 6){
+        image1.src = './images/dice6.png';
+    }
+    /*image changing for player 2 */
+    if(randNum2 == 1){
+        image2.src = './images/dice1.png';
+    } else if(randNum2 == 2){
+        image2.src = './images/dice2.png';
+    } else if(randNum2 == 3){
+        image2.src = './images/dice3.png';
+    } else if(randNum2 == 4){
+        image2.src = './images/dice4.png';
+    } else if(randNum2 == 5){
+        image2.src = './images/dice5.png';
+    } else if(randNum2 == 6){
+        image2.src = './images/dice6.png';
+    }
+    if(randNum1 >randNum2 ){
+        document.querySelector("#winnerfr").innerHTML = "Player 1 wins!";
+    } else if(randNum1 < randNum2){
+        document.querySelector("#winnerfr").innerHTML = "Player 2 wins!";
+    } else{
+        document.querySelector("#winnerfr").innerHTML = "It's a Draw!";
+    }
+    
+}
